7e64017f2b9d80b0c8840a2af520e6254cbaff9b
HohnSolo <davidhohn1@gmail.com>
2024-04-09 15:53:35 -0700

    adding java file

---
+++ PriorityQueue.java
@@ -0,0 +1,35 @@
+/*
+TCSS 342
+Author: David Hohn
+This file initializes and performs the functions for the queue.
+ */
+
+public class PriorityQueue<T extends Comparable<T>> extends ArrayHeap<T> {
+
+    // initialize the priority queue
+    public PriorityQueue() {
+        super();
+    }
+
+    // initialize with given capacity
+    public PriorityQueue(int capacity) {
+        super(capacity);
+    }
+
+    // insert a new elem into the queue
+    public void insert(T elem) {
+        addElement(elem);
+    }
+
+    // remove and return the elem with the highest priority, which should be min elem
+    public T remove() {
+        return removeMin();
+    }
+
+    // return a string of the queue
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
